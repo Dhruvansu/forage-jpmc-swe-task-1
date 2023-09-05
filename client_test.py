412a9be1f8b32ae12de16ee8a78c0563d63f1cf9
Dhruvansu Thakkar <dhruvanshuthakkar@gmail.com>
2023-09-05 10:54:13 -0400

    FIX(test): add price_a equal to zero check for getRatio method in client.py file and add test cases relating to the check

--- client_test.py
+++ client_test.py
@@ -23,8 +23,24 @@ class ClientTest(unittest.TestCase):
       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculatePriceZeroForABCStockPrice_a(self):
+    quotes = [
+      # {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 0.0, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0.0, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
 
-  def test_getRatio_calculatePriceZeroForDEFStock(self):
+    prices = {}
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      prices[stock] = price
+    
+    # expect getRatio funtion to return None then the test passes which means price of a stock is 0 and getRatio function caught it
+    # to fail this test comment out the ABC stock whose price is zero and uncomment the ABC stock whose prices are correct 
+    self.assertEqual(getRatio(prices['ABC'], prices["DEF"]), None)
+
+  def test_getRatio_calculatePriceZeroForDEFStockPrice_b(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 0.0, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0.0, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
@@ -37,7 +53,7 @@ class ClientTest(unittest.TestCase):
       stock, bid_price, ask_price, price = getDataPoint(quote)
       prices[stock] = price
     
-    # expect getRation funtion to return None and if None == None then test passes which means price of a stock is 0 and getRation function caught it
+    # expect getRatio funtion to return None then the test passes which means price of a stock is 0 and getRatio function caught it
     # to fail this test comment out the DEF stock whose price is zero and uncomment the DEF stock whose prices are correct 
     self.assertEqual(getRatio(prices['ABC'], prices["DEF"]), None)
 
